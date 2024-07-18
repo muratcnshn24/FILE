111b101b617d67583435384fe5a957af8b32461d
muratcnshn24 <127681337+muratcnshn24@users.noreply.github.com>
2024-07-18 19:08:47 +0300

    Proje dosyası ekle.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace FILE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // Dinamik konum
+                string[] yol = Directory.GetCurrentDirectory().Split('\\');
+                string dosyaYol = " ";
+                for (int i = 0; i < yol.Length - 3; i++)
+                    dosyaYol += yol[i] + '\\';
+                dosyaYol += "Rakamlar.txt";
+
+
+                // Dosya Okuma ve yazma
+                using(StreamReader sr= new StreamReader(dosyaYol))
+                {
+
+                    string line;
+                    while ((line= sr.ReadLine())!=null)
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+          
+            }
+        }
+    }
+}
